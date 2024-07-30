6598acd3cb0bd933277282da504ab4f0a6a9f4bb
Ros2 <dbwodud5942@naver.com>
2024-07-30 15:50:43 +0900

    hh

--- build/simple_pkg_cpp/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/lab/ros2_ws/build/simple_pkg_cpp/helloworld" "TARGETS" "helloworld" "DESTINATION" "lib/simple_pkg_cpp")
--- install/simple_pkg_cpp/share/simple_pkg_cpp/cmake/simple_pkg_cppConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/lab/ros2_ws/build/simple_pkg_cpp/ament_cmake_core/simple_pkg_cppConfig-version.cmake
