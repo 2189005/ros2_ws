e3e8f64f076e96a67591c8dd387f1b081ecabe34
Ros2 <dbwodud5942@naver.com>
2024-08-01 17:10:55 +0900

    aa

--- move_t_sim/src/moveTurtleBotcpp.cpp
+++ move_t_sim/src/moveTurtleBotcpp.cpp
@@ -10,12 +10,16 @@ MoveTurtleBot::MoveTurtleBot()
     _timer = this->create_wall_timer(30ms, std::bind(&MoveTurtleBot::publish_turtlesim_msg, this));
 }
 
-void MoveTurtleBot::publish_turtlesim_msg(){
+void MoveTurtleBot::publish_turtlesim_msg()
+{
     auto twist = geometry_msgs::msg::Twist();
     twist.linear.x = 0.1;
-    twist.angular.z = 0.5;
+    twist.angular.z = 1.0;
     _twist_pub->publish(twist);
 }
-void MoveTurtleBot::sub_odom_msg(const nav_msgs::msg::Odometry::SharedPtr msg){
-    RCLCPP_INFO(get_logger(), "I heard: [%f]", msg->twist.twist.linear.x);
+
+void MoveTurtleBot::sub_odom_msg(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    _odom_msg = *msg;
+    RCLCPP_INFO(get_logger(), "Position(x: %f, y: %f, z: %f)", _odom_msg.pose.pose.position.x, _odom_msg.pose.pose.position.y, _odom_msg.pose.pose.position.z);
 }
